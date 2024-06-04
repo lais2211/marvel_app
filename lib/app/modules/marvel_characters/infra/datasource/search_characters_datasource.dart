8236032884383d325786162b4b1fab81af2f58c3
lais2211 <lais.sousa1608@gmail.com>
2024-06-04 18:43:44 -0300

    feat: criação do datasource na infra

---
+++ lib/app/modules/marvel_characters/infra/datasource/search_characters_datasource.dart
@@ -0,0 +1,5 @@
+import 'package:marvel_app/app/modules/marvel_characters/infra/models/result_characters_model.dart';
+
+abstract class SearchCharactersDatasource {
+  Future<List<ResultCharactersModel>> getCharacters();
+}
