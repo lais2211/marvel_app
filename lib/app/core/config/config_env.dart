bd709837b2c5ba8938c0e0b02c4bf74fe9c567bf
lais2211 <lais.sousa1608@gmail.com>
2024-06-04 23:20:13 -0300

    feat: adição de pasta .env para segurança de dados de configuração

--- lib/app/core/config/config_env.dart
+++ lib/app/core/config/config_env.dart
@@ -1,6 +1,8 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class ConfigEnv {
   static String basePath = 'http://gateway.marvel.com/';
   static String charactersPath = 'v1/public/characters';
-  static String apiKey = '09a41d406f7d276ac0cb76828c359ba5';
-  static String hash = 'ba5efd3332671bda3de42c8a23f11dc4';
+  static String apiKey = dotenv.env['API_KEY']!;
+  static String hash = dotenv.env['API_HASH']!;
 }
