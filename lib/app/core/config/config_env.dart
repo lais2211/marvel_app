e782baadad3fcea81e95470d628ea232e90641b6
lais2211 <lais.sousa1608@gmail.com>
2024-06-04 22:19:04 -0300

    feat: criação de arquivos de configuração

---
+++ lib/app/core/config/config_env.dart
@@ -0,0 +1,6 @@
+class ConfigEnv {
+  static String basePath = 'http://gateway.marvel.com/';
+  static String charactersPath = 'v1/public/characters';
+  static String apiKey = '09a41d406f7d276ac0cb76828c359ba5';
+  static String hash = 'ba5efd3332671bda3de42c8a23f11dc4';
+}
---
+++ lib/app/core/mocks/mock_config_env.dart
@@ -0,0 +1,20 @@
+class MockConfigEnv {
+  static Map<String, Object> marvelCharactersResult = {
+    "code": 200,
+    "status": "Ok",
+    "data": {
+      "results": [
+        {
+          "id": 1011334,
+          "name": "3-D Man",
+          "description": "",
+          "modified": "2014-04-29T14:18:17-0400",
+          "thumbnail": {
+            "path": "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784",
+            "extension": "jpg"
+          }
+        }
+      ]
+    }
+  };
+}
